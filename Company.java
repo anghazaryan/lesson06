6ba99a52fbbff235671b5f030f533858f6fb28a7
anghazaryan <anahit.ghazaryan91@gmail.com>
2023-06-29 00:57:50 +0400

    created a company

--- Company.java
+++ Company.java
@@ -1,17 +1,5 @@
 public class Company {
 
-// Create a Employee class with salary and years of experience
-// Create a Company class with Name, moneyInBank and employees
-// Create employees 
-// Create company and assign the created employees to the company
-// Write a method, to calculate whether Company can afford employees
-
-	// պետք ա ունենանք company ու employee class եր, 
-	// հետո ստեղծենք կոնկրետ մի հատ company ու մի քանի հատ employee ներից կազմված array, 
-	// հետո էդ կոնկրետ company -ում պետք ա հաշվենք իրա բանկի փողը հերիք ա, 
-	// էդ array մեջի աշխատողների աշխատավարձը տալու համար թե չէ
-
-
 	private String companyName;
 	private double moneyInBank;
 	private Employee[] employees;
